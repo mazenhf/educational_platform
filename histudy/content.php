87b7b03bde83e59233956b306c14adfbebd940e6
Mazen <mazenhajfraj8@gmail.com>
2023-05-28 03:31:38 +0100

    modifications

---
+++ histudy/content.php
@@ -0,0 +1,76 @@
+ <div class="rbt-overlay-page-wrapper">
+
+        <div class="breadcrumb-image-container breadcrumb-style-max-width">
+            <div class="breadcrumb-image-wrapper">
+                <img src="https://unoeilsur.fr/assets/images/bg/bg-image-10.jpg" alt="Fond UnOeilSur.fr">
+            </div>
+            <div class="breadcrumb-content-top text-center">
+                <h1 class="title">Politique de confidentialité</h1>
+                <p class="mb--20">du site internet UnOeilSur.fr</p>
+                <ul class="page-list">
+                    <li class="rbt-breadcrumb-item"><a href="/">Accueil</a></li>
+                    <li>
+                        <div class="icon-right"><i class="feather-chevron-right"></i></div>
+                    </li>
+                    <li class="rbt-breadcrumb-item active">Politique de confidentialité</li>
+                </ul>
+            </div>
+        </div>
+
+
+        <div class="rbt-putchase-guide-area breadcrumb-style-max-width rbt-section-gapBottom">
+            <div class="rbt-article-content-wrapper">
+                <div class="content">
+<p>La présente politique de confidentialité a pour objectif d'informer les utilisateurs du site internet <a href="https://unoeilsur.fr/" target="_new">https://unoeilsur.fr</a> (ci-après dénommé "le Site") sur la manière dont leurs données personnelles sont collectées, traitées et protégées. Nous accordons une grande importance à la protection de la vie privée de nos utilisateurs et nous nous engageons à respecter les dispositions du Règlement général sur la protection des données (RGPD) et toute autre législation applicable en matière de protection des données personnelles.</p>
+<h3>Responsable du traitement des données</h3>
+<p>Le responsable du traitement des données personnelles collectées sur le Site est UnOeilSur.fr, dont le siège social est situé à 14 Rue du Colisée, 75008 Paris France, représenté par Sandra Teani et Emmanuel Daluz. Pour toute question concernant la protection de vos données personnelles, vous pouvez nous contacter par e-mail à l'adresse suivante : contact@unoeilsur.fr.</p>
+<h3>Données collectées et finalités du traitement</h3>
+<p>Nous collectons différentes catégories de données personnelles sur le Site, notamment :</p>
+<ul>
+ 	<li>Données d'identification : nom, prénom, adresse e-mail, etc.</li>
+ 	<li>Données de connexion : adresse IP, cookies, etc.</li>
+ 	<li>Données de navigation : pages visitées, durée de la visite, etc.</li>
+</ul>
+<p>Les données personnelles collectées sont utilisées pour les finalités suivantes :</p>
+<ul>
+ 	<li>Gestion de votre compte utilisateur</li>
+ 	<li>Envoi de newsletters et d'informations sur nos services</li>
+ 	<li>Amélioration de nos services et de votre expérience utilisateur</li>
+ 	<li>Réalisation d'études statistiques et d'analyses de fréquentation du Site</li>
+ 	<li>Gestion des demandes d'information et des réclamations</li>
+</ul>
+<h3>Destinataires des données</h3>
+<p>Vos données personnelles sont traitées par le personnel habilité d'UnOeilSur.fr et, le cas échéant, par des sous-traitants et partenaires contractuellement liés à UnOeilSur.fr et soumis aux mêmes obligations de confidentialité et de sécurité.</p>
+<h3>Durée de conservation des données</h3>
+<p>Vos données personnelles sont conservées pendant la durée nécessaire à l'accomplissement des finalités pour lesquelles elles ont été collectées, dans le respect des délais de prescription légaux.</p>
+<h3>Sécurité des données</h3>
+<p>Nous mettons en œuvre des mesures techniques et organisationnelles appropriées pour protéger vos données personnelles contre toute destruction, perte, altération, divulgation ou accès non autorisé.</p>
+<h3>Vos droits</h3>
+<p>Conformément au RGPD et aux lois applicables, vous disposez des droits suivants concernant vos données personnelles :</p>
+<ul>
+ 	<li>Droit d'accès</li>
+ 	<li>Droit de rectification</li>
+ 	<li>Droit à l'effacement</li>
+ 	<li>Droit à la limitation du traitement</li>
+ 	<li>Droit d'opposition</li>
+ 	<li>Droit à la portabilité des données</li>
+</ul>
+<p>Pour exercer ces droits, vous pouvez nous contacter à l'adresse e-mail indiquée au point 2 de cette politique de confidentialité.</p>
+<h3>Modifications de la politique de confidentialité</h3>
+<p>Nous nous réservons le droit de modifier la présente politique de confidentialité à tout moment. Nous vous invitons à la consulter régulièrement afin de prendre connaissance des éventuelles mises à jour.</p>
+<h3>Contact</h3>
+<p>Pour toute question concernant notre politique de confidentialité, n'hésitez pas à nous contacter à l'adresse e-mail indiquée au point 2 de cette politique de confidentialité ou par courrier postal à l'adresse suivante :</p>
+<p><strong>UnOeilSur.fr</strong></p>
+<p><strong>14 Rue du Colisée, 75008 Paris</strong></p>
+<p><strong>France</strong></p>
+<h3>Autorité de contrôle</h3>
+<p>Si vous estimez que le traitement de vos données personnelles viole les dispositions du RGPD ou toute autre législation applicable, vous avez le droit d'introduire une réclamation auprès de l'autorité de contrôle compétente. En France, cette autorité est la Commission Nationale de l'Informatique et des Libertés (CNIL).</p>
+<p>Adresse : CNIL 3 Place de Fontenoy TSA 80715 75334 Paris Cedex 07 France</p>
+<p>Téléphone : +33 (0)1 53 73 22 22</p>
+<p>Site internet : <a href="https://www.cnil.fr/" target="_new">https://www.cnil.fr</a></p>
+<p>Dernière mise à jour : 11/04/2023</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
